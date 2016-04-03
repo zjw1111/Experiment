cf17c53218746fc0b93994438c9aa80cec62e17b
kopyh <983661601@qq.com>
2016-04-03 23:31:32 +0800

    updata dataStructure 2

--- dataStructureExperiment/2-JosephRing.cpp
+++ dataStructureExperiment/2-JosephRing.cpp
@@ -1,56 +1,59 @@
 //kopyh
 #include <bits/stdc++.h>
-using namespace std;
-//双向链表节点存储每个人的编号
 struct node
 {
-    int num;
-    node *next, *prior;
-    node(int x, node *p = NULL, node *q = NULL):num(x),next(p),prior(q){}
+    int num,pwd;
+    node *next;
+    node(int x, node *p = NULL):num(x),next(p){}
 };
-//初始化所有人形成一个环
-void init(int n, node *start)
-{
-    int i=2;
-    node *tmp = start;
-    while(i<=n)
-    {
-        tmp->next = new node(i++);
-        tmp->next->prior = tmp;
-        tmp = tmp->next;
-    }
-    tmp->next = start;
-    start->prior = tmp;
-}
-//按序输出答案
-void solve(int n, int m, node *start)
-{
-    int i=0,j=1;
-    while(i<n)
-    {
-        if(j == m)
-        {
-            if(i!=0)printf(" -> ");
-            printf("%d",start->num);
-            start->prior->next = start->next;
-            start->next->prior = start->prior;
-            start = start->next;
-            j = 1;
-            i++;
-        }
-        else
-            start = start->next,j++;
-    }
-    printf("\n");
-}
 int main()
 {
-    int n,m;
-    node *start = new node(1);
-    printf("Please input the number of people and the number of password:\n");
-    scanf("%d%d",&n,&m);
-    init(n,start);
+    int n,m,i,j;
+    char s[10];
+    printf("Please input the number of people:\n");
+    scanf("%d",&n);
+    printf("Everyone's password is same?(Y/N)\n");
+    scanf("%s",s);
+    int flag=(strcmp(s,"Y")==0?1:0);
+    printf("Please input password:\n");
+
+    //init
+    if(flag)scanf("%d",&m);
+    node *p, *q, *start;
+    i=1;
+    while(i<=n)
+    {
+        p = new node(i++);
+        if(flag)p->pwd = m;
+        else scanf("%d",&p->pwd);
+        if(i==2)start = p, m = start->pwd;
+        else q->next = p;
+        q = p;
+    }
+    p = start;
+    q->next = p;
+
+    //solve
     printf("Output order is:\n");
-    solve(n,m,start);
+    i=1,j=0;
+    while(j<n)
+    {
+        if(i == m)
+        {
+            if(j)printf(" -> ");
+            printf("%d",p->num);
+            m = p->pwd;
+            q->next = p->next;
+            delete(p);
+            p = q->next;
+            j++;
+            i = 1;
+        }
+        else
+            i++, p=p->next, q=q->next;
+    }
+    printf("\n");
+
     return 0;
 }
+
