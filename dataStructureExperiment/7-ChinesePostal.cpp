943f231f26cd8ada1569d8f2abd79e85249a4d31
kopyh <983661601@qq.com>
2016-04-26 12:09:53 +0800

    updata dataStructure 7“

--- dataStructureExperiment/7-ChinesePostal.cpp
+++ dataStructureExperiment/7-ChinesePostal.cpp
@@ -8,25 +8,31 @@ class ChinesePostal
 private:
     //邮路节点数和路径数
     int n,m;
-    //节点间路径个数及路径长度
+    //邻接矩阵存图
     int g[N][N],f[N][N];
-    int tmp[N],ans[N],pos[N],res,vis[N],sum,prior;
-    struct node
+    //邻接表存图
+    struct edge
     {
         int v;
-        int c;
+        int cost;
+        edge(int _v=0,int _cost=0):v(_v),cost(_cost) {}
+    };
+    vector<edge>E[N];
+    //记录奇数点两两配对时的组合及最小增量
+    int tmp[N],ans[N],res,vis[N];
+    //记录奇数点位置及个数
+    int pos[N],sum;
+
+    //Dijkstra计算最短路
+    struct node
+    {
+        int v,c;
         node(int _v=0,int _c=0):v(_v),c(_c) {}
         bool operator <(const node &r)const
         {
             return c>r.c;
         }
     };
-    struct edge
-    {
-        int v;int cost;
-        edge(int _v=0,int _cost=0):v(_v),cost(_cost) {}
-    };
-    vector<edge>E[N];
     int dist[N],ff[N];
     vector<int>road[N][N];
     void addedge(int u,int v,int w)
@@ -36,12 +42,11 @@ private:
         road[u][v].push_back(0);
         road[v][u].push_back(0);
     }
-    //Dijkstra计算最短路
     void Dijkstra(int start)
     {
         memset(vis,false,sizeof(vis));
-        for(int i=1;i<=n;i++)dist[i]=INF;
-        for(int i=1;i<=n;i++)ff[i]=i;
+        for(int i=1; i<=n; i++)dist[i]=INF;
+        for(int i=1; i<=n; i++)ff[i]=i;
         priority_queue<node>q;
         while(!q.empty())q.pop();
         dist[start]=0.0;
@@ -69,7 +74,7 @@ private:
     }
     void putRoad(int x)
     {
-        for(int i=1;i<=n;i++)
+        for(int i=1; i<=n; i++)
             if(x!=i && road[x][i].empty())
             {
                 int y=ff[i];
@@ -78,10 +83,11 @@ private:
                     road[i][x].push_back(y);
                     y=ff[y];
                 }
-                for(int j=road[i][x].size()-1;j>=0;j--)
+                for(int j=road[i][x].size()-1; j>=0; j--)
                     road[x][i].push_back(road[i][x][j]);
             }
     }
+
     //寻找最优二分点对组合
     void tryTo(int u,int flag,int now,int val)
     {
@@ -89,17 +95,18 @@ private:
         {
             if(res>val)
             {
-                val=res;
+                res=val;
                 memcpy(ans,tmp,sizeof(tmp));
             }
             return;
         }
-        for(int i=0;i<sum;i++)
+        for(int i=0; i<sum; i++)
             if(!vis[pos[i]])
             {
                 if(flag)
                 {
-                    tmp[u]=pos[i];tmp[pos[i]]=u;
+                    tmp[u]=pos[i];
+                    tmp[pos[i]]=u;
                     vis[pos[i]]=1;
                     tryTo(pos[i],0,now+1,val+f[u][pos[i]]);
                     vis[pos[i]]=0;
@@ -115,24 +122,25 @@ private:
     //补边构建欧拉图
     void operate()
     {
-        for(int i=0;i<sum;i++)
+        for(int i=0; i<sum; i++)
         {
             Dijkstra(pos[i]);
-            for(int j=1;j<=n;j++)
+            for(int j=1; j<=n; j++)
                 f[pos[i]][j]=f[j][pos[i]]=dist[j];
             putRoad(pos[i]);
         }
         memset(vis,false,sizeof(vis));
         res=INF;
         tryTo(0,0,0,0);
-        for(int i=0;i<sum;i++)
+        for(int i=0; i<sum; i++)
             g[pos[i]][ans[pos[i]]]++;
     }
+
     //fleury算法寻找欧拉回路
     stack<int> st;
     void dfs(int u)
     {
-        for(int i=1;i<=n;i++)
+        for(int i=1; i<=n; i++)
             if(g[u][i])
             {
                 g[u][i]--,g[i][u]--;
@@ -141,6 +149,7 @@ private:
                 break;
             }
     }
+    int prior;//前序节点
     void fleury(int u)
     {
         while(!st.empty())st.pop();
@@ -148,7 +157,7 @@ private:
         while(!st.empty())
         {
             int flag=1;
-            for(int i=1;i<=n;i++)
+            for(int i=1; i<=n; i++)
                 if(g[st.top()][i])
                 {
                     flag=0;
@@ -156,10 +165,11 @@ private:
                 }
             if(flag)
             {
-                int t=st.top(); st.pop();
+                int t=st.top();
+                st.pop();
                 if(prior && road[prior][t][0]!=0)
                 {
-                    for(int i=0;i<road[prior][t].size();i++)
+                    for(int i=0; i<road[prior][t].size(); i++)
                         printf("%d ",road[prior][t][i]);
                 }
                 printf("%d ",t);
@@ -169,13 +179,14 @@ private:
                 dfs(st.top());
         }
     }
+
     //判断奇数点个数判断是否需要补边
     void solve()
     {
-        for(int i=1;i<=n;i++)
+        for(int i=1; i<=n; i++)
         {
             int now=0;
-            for(int j=1;j<=n;j++)
+            for(int j=1; j<=n; j++)
                 now+=g[i][j];
             if(now&1)pos[sum++]=i;
         }
@@ -187,11 +198,12 @@ private:
     {
         memset(g,0,sizeof(g));
         memset(f,0x3f,sizeof(f));
-        for(int i=1;i<=n;i++)E[i].clear();
-        for(int i=1;i<=n;i++)
-            for(int j=1;j<=n;j++)
+        for(int i=1; i<=n; i++)E[i].clear();
+        for(int i=1; i<=n; i++)
+            for(int j=1; j<=n; j++)
                 road[i][j].clear();
-        prior=0;sum=0;
+        prior=0;
+        sum=0;
     }
 public:
     void input()
@@ -199,7 +211,7 @@ public:
         int x,y,z;
         scanf("%d%d",&n,&m);
         init();
-        for(int i=0;i<m;i++)
+        for(int i=0; i<m; i++)
         {
             scanf("%d%d%d",&x,&y,&z);
             g[x][y]=g[y][x]=1;
@@ -208,7 +220,7 @@ public:
         }
         solve();
     }
-}CP;
+} CP;
 
 int main()
 {
